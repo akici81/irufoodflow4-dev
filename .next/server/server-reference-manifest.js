ffbb49bef7d0f10c052e97621be3673f12dfcb1e
akici81 <81akici81@gmail.com>
2026-02-24 09:11:00 +0300

    fix: remove tracked .next and .vscode from repo

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pM9FMpojf1/7Uhp81mNRnC4Y4uGHCImvzTiSA7w3tAQ=\"}"
--- .next/static/chunks/181.fb43b5bf2b297264.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[181],{6786:function(){},543:function(){}}]);
